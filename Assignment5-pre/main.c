5cb94dd109d1c9b235d472f4511f31efdd2465ed
FullAlternate <33181597+FullAlternate@users.noreply.github.com>
2017-10-29 14:06:11 +0100

    Update main.c

--- Assignment5-pre/main.c
+++ Assignment5-pre/main.c
@@ -55,11 +55,11 @@ void bouncing_balls(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Surface *s
     time_t t;
     srand((unsigned) time(&t));
     object_t *object = create_object(surface, sphere_model, SPHERE_NUMTRIANGLES );
-    object_t *object1 = create_object(surface, sphere_model, SPHERE_NUMTRIANGLES );
+    /*object_t *object1 = create_object(surface, sphere_model, SPHERE_NUMTRIANGLES );
     object_t *object2 = create_object(surface, sphere_model, SPHERE_NUMTRIANGLES );
     object_t *object3 = create_object(surface, sphere_model, SPHERE_NUMTRIANGLES );
     object_t *object4 = create_object(surface, sphere_model, SPHERE_NUMTRIANGLES );
-    object_t *iterObject = NULL;
+    */object_t *iterObject = NULL;
 
     list_t *theList = list_create();
     list_iterator_t *theIter = list_createiterator(theList);
@@ -72,7 +72,7 @@ void bouncing_balls(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Surface *s
 
     object->ty = radius;
     object->tx = radius;
-    object1->ty = radius;
+    /*object1->ty = radius;
     object1->tx = radius;
     object2->ty = radius;
     object2->tx = radius;
@@ -80,13 +80,13 @@ void bouncing_balls(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Surface *s
     object3->tx = radius;
     object4->ty = radius;
     object4->tx = radius;
-
+*/
     list_addlast(theList, object);
-    list_addlast(theList, object1);
+    /*list_addlast(theList, object1);
     list_addlast(theList, object2);
     list_addlast(theList, object3);
     list_addlast(theList, object4);
-
+*/
     float gravity = 0.1;
     float boost = 0.8;     
     float maxspeed = 100;
@@ -185,8 +185,14 @@ void bouncing_balls(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Surface *s
                     accelerate_object(iterObject,boost, maxspeed);
                     iterObject->speedy = iterObject->speedy * -1;
                 }
-                if(iterObject->speedx && iterObject->speedy == 0){
+                printf("%f | %f", iterObject->speedx, iterObject->speedy);
+                if(iterObject->speedx  < abs(0.000001)){
+                    iterObject->model = NULL;
+                    destroy_object(iterObject);
                     list_remove(theList, iterObject);
+                  
+                
+
                 }
             }
             
